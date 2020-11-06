c8242b701fd10923929fd3f07ade1824ac8fbbb7
James <james.loizou@nology.io>
2020-11-06 10:52:21 +0000

    working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import "./App.css";
+
 const App = () => {
   // Store our data/collections here for us to show
   const [data, setData] = useState([]); 
